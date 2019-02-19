0b0ed941082d8544934783bd599520d5d2f0e729
Nikhil Vijayan <nkhilv@gmail.com>
2019-02-19 16:05:44 +0000

    Add toContain matcher

--- src/matchers.js
+++ src/matchers.js
@@ -2,4 +2,7 @@ const matchers = value => ({
   toEqual(expectation) {
     console.log(value == expectation);
   },
+  toContain(expectation) {
+    console.log(value.includes(expectation));
+  }
 });
