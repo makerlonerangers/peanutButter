7bbdb6eceafb472c4fe9ee193ccf32488f880749
JPProgrammingTime <41450070+JPProgrammingTime@users.noreply.github.com>
2019-02-20 13:49:09 +0000

    Add toThrowError and toThrowErrorMessage matchers

--- src/matchers.js
+++ src/matchers.js
@@ -2,6 +2,24 @@ const matchers = value => ({
   toEqual(expectation) {
     console.log(value == expectation);
   },
+  toThrowError() {
+    try {
+      value();
+    } catch (error) {
+      console.log(true);
+    }
+  },
+  toThrowErrorMessage(errorMessage) {
+    try {
+      value();
+    } catch (error) {
+      if (errorMessage === error.message) {
+        console.log(true);
+      } else {
+        console.log(false);
+      }
+    }
+  },
   toContain(expectation) {
     console.log(value.includes(expectation));
   }
--- src/test.js
+++ src/test.js
@@ -3,6 +3,17 @@ describe('peanutButter Tests', () => {
     expect(2).toEqual(2);
   });
 
+  it('throw error test', () => {
+    expect(() => {
+      throw new Error('Fail');
+    }).toThrowError();
+  });
+
+  it('throw error message test', () => {
+    expect(() => {
+      throw new Error('Fail');
+    }).toThrowErrorMessage('Fail');
+    
   it('tests toContain', () => {
     expect([1, 2, 3]).toContain(2);
   });
