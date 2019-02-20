ede35719ded024fcff6e37c49df36e4d00a66108
Jiseong Park <41450070+JPProgrammingTime@users.noreply.github.com>
2019-02-20 10:30:00 +0000

    Add toThrowError matcher.

--- src/matchers.js
+++ src/matchers.js
@@ -2,4 +2,16 @@ const matchers = value => ({
   toEqual(expectation) {
     console.log(value == expectation);
   },
+
+  toThrowError(errorMessage) {
+    try {
+      value();
+    } catch (error) {
+      if (errorMessage === error.message) {
+        console.log(true);
+      } else {
+        console.log(false);
+      }
+    }
+  },
 });
--- src/test.js
+++ src/test.js
@@ -2,4 +2,10 @@ describe('peanutButter Tests', () => {
   it('singular test', () => {
     expect(2).toEqual(2);
   });
+
+  it('throw error test', () => {
+    expect(() => {
+      throw new Error('Fail');
+    }).toThrowError('Fail');
+  });
 });
